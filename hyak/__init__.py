2082bcdce3ea0fea634e1acf4b33bb26fb9d8aa2
Rodrigo Luger <rodluger@gmail.com>
2015-10-30 15:21:55 -0700

    add setup

--- hyak/__init__.py
+++ hyak/__init__.py
@@ -5,3 +5,8 @@ from __future__ import division, print_function, absolute_import, unicode_litera
 
 from .decorators import MPI, MULTI
 from .pbs import qsub
+
+# Info
+__version__ = "0.0.1"
+__author__ = "Rodrigo Luger (rodluger@uw.edu)"
+__copyright__ = "Copyright 2015 Rodrigo Luger"
